3616a5570ab5e2605dfdc813d55fc86c45991663
Matthias Harrer <matthias.harrer@trumpf.com>
2019-09-04 17:59:07 +0200

    few more steps

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.end("Welcome to this short little quiz. To start just post your name.")
+    res.end(`<html><head><title>Next steps</title></head><body>
+                Welcome to this short little quiz. To start just post your name.
+                <form action="/wrong-post" method="get">
+                    <input type="text" placeholder="Username" name="username">
+                    <input type="submit" value="Submit">
+                </form>
+            </body></html>`)
 });
 
-app.post("/", (req, res) => {
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    res.location(path.join(fullUrl, "next-steps"))
-       .end(`Hi ${req.body}!`);
+app.get("/wrong-post", (req, res) => {
+    if(!req.query.username) {
+        return res.status(400).end();
+    }
+
+    res.end(`Nice to meet you ${req.query.username}, but this was not the correct way to do it (and actually a GET and not a POST). Please use an HTTP Client, for example the 'REST Client' Extension for VS Code or Postman and try again.`)
 })
+
+app.post("/", bodyParser.text(), (req, res) => {
+    var nextStepsUrl = 'https://' + req.get('host') + path.join(req.originalUrl, "next-steps");
+    res.location(nextStepsUrl)
+       .end(`Hi ${req.body}!\nWhen creating new resources using POST the server usually returns the location of the created resource in the 'Location' Header`);
+});
+
+app.get("/next-steps", (req, res) => {
+    res.send("Hey you've found it! Congratulations. The next step should be that you check out my options.");
+});
+
+app.options("/next-steps", (req, res) => {
+    res.header('Allow', 'GET,DELETE');
+    res.send("Look at the allow response header.");
+});
+
+app.delete("/next-steps", (req, res) => {
+    if(req.headers.authorization === "Bearer 8a5ed8b0b3ed4d698e52ee7d14ed405d") {
+        res.send("Oh no .. all the steps are gone now.")
+    } else {
+        res.send(`You need to authenticate in order to call this method. This can be done by sending "Bearer 8a5ed8b0b3ed4d698e52ee7d14ed405d" in the "Authorization" Header`);
+    }
+});
+
+app.get("*", (req, res) => {
+    res.end("Nice try ...")
+});
 
 app.listen(process.env.PORT || 80, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
